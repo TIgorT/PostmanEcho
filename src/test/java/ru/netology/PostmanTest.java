75d0c7ce4cc8fb9cf8235399269a47ca6865bbcd
Igor <TimoshenkoII@yandex.com>
2023-05-24 21:06:32 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void postRequestTestFirst(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Что то этот блок мне даётся очень сложно") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(400)
+                .body("data", equalTo("Что то этот блок мне даётся очень сложно"));
+    }
+}
+
